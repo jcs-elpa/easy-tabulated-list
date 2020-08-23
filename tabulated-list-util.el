24d4bbd4d6c9b9225076c278bbc88dc72a25905d
jenchieh <jcs090218@gmail.com>
2020-08-23 19:57:50 +0800

    Fist core.

---
+++ tabulated-list-util.el
@@ -0,0 +1,38 @@
+;;; tabulated-list-util.el --- Simplify usage for `tabulated-list'  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Shen, Jen-Chieh
+;; Created date 2020-08-23 17:15:12
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Simplify usage for `tabulated-list'.
+;; Keyword: tabulated list table usage simplify
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs090218/tabulated-list-util
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Simplify usage for `tabulated-list'.
+;;
+
+;;; Code:
+
+
+
+(provide 'tabulated-list-util)
+;;; tabulated-list-util.el ends here
